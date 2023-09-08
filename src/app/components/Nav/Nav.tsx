7525b72e1bd20d263f643273f1d673bd4e48f886
Chris Ware <c.p.ware@hotmail.com>
2023-09-07 19:45:30 -0700

    Nav Start

--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import styles from "./Nav.module.css";
 import Link from "next/link";
+import Logo from "../../../../public/icons/nierlogo.png";
+import Image from "next/image";
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -38,7 +40,109 @@ const Nav = () => {
     <header className={styles.header}>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
-          <Link href='/'>NIER</Link>
+          <Link href='/' className={styles.logo}>
+            <Image
+              src={Logo}
+              alt='logo'
+              width={107}
+              height={50}
+              className={styles.officialLogo}
+            />
+          </Link>
+        </div>
+        <div className={styles.menuItemsGroup}>
+          <ul
+            className={
+              isOpen === false
+                ? styles.navMenu
+                : styles.navMenu + " " + styles.active
+            }
+          >
+            <li onClick={openMenu}>
+              {/* <Link href='/' className={styles.logo}>
+                <Logo className={styles.officialLogoMobile} />
+              </Link> */}
+            </li>
+            <li className={styles.navItem} onClick={openMenu}>
+              <Link
+                href='/'
+                className={pathname === "/" ? styles.activeLink : ""}
+              >
+                Home
+              </Link>
+            </li>
+            <li className={styles.navItem} onClick={openMenu}>
+              <Link
+                href='/about'
+                className={pathname.includes("/about") ? styles.activeLink : ""}
+              >
+                About
+              </Link>
+            </li>
+            <li className={styles.navItem} onClick={openMenu}>
+              <Link
+                href='/services'
+                className={
+                  pathname.includes("/services") ? styles.activeLink : ""
+                }
+              >
+                Services
+              </Link>
+            </li>
+            <li className={styles.navItem} onClick={openMenu}>
+              <Link
+                href='/pricing'
+                className={
+                  pathname.includes("/pricing") ? styles.activeLink : ""
+                }
+              >
+                Pricing
+              </Link>
+            </li>
+            <li className={styles.navItem} onClick={openMenu}>
+              <Link
+                href='/faq'
+                className={pathname.includes("/faq") ? styles.activeLink : ""}
+              >
+                FAQ
+              </Link>
+            </li>
+            <li className={styles.navItem} onClick={openMenu}>
+              <Link
+                href='/blog'
+                className={pathname.includes("/blog") ? styles.activeLink : ""}
+              >
+                Blog
+              </Link>
+            </li>
+            <li className={styles.navItem} onClick={openMenu}>
+              <Link
+                href='/contact'
+                className={
+                  pathname.includes("/contact") ? styles.activeLink : ""
+                }
+              >
+                Contact
+              </Link>
+            </li>
+            <div className={styles.bottom}>
+              <p className={styles.copy}>
+                Copyright &copy; 2023 Chris Ware Agency || All Rights Reserved
+              </p>
+            </div>
+          </ul>
+          <span
+            className={
+              isOpen === false
+                ? styles.hamburger
+                : styles.hamburger + " " + styles.active
+            }
+            onClick={openMenu}
+          >
+            <span className={styles.whiteBar}></span>
+            <span className={styles.whiteBar}></span>
+            <span className={styles.whiteBar}></span>
+          </span>
         </div>
       </nav>
     </header>
