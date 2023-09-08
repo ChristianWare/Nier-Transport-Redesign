2839d853132b0dcb5586ad1dda228215514b8d3e
Chris Ware <c.p.ware@hotmail.com>
2023-09-08 16:00:33 -0700

    About Section Done

--- src/app/components/AboutUsSection/AboutUsSection.tsx
+++ src/app/components/AboutUsSection/AboutUsSection.tsx
@@ -2,6 +2,10 @@ import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
 import styles from "./AboutUsSection.module.css";
 import Image from "next/image";
 import Img1 from "../../../../public/images/img1.png";
+import Driver from "../../../../public/icons/driver.svg";
+import Options from "../../../../public/icons/options.svg";
+import Diamond from "../../../../public/icons/diamond.svg";
+import Button from "../Button/Button";
 
 const AboutUsSection = () => {
   return (
@@ -14,8 +18,8 @@ const AboutUsSection = () => {
             </div>
           </div>
           <div className={styles.right}>
-            <h2>A Company You Can Trust To Be With You At Every Step</h2>
-            <p>
+            <h2 className={styles.heading}>About us</h2>
+            <p className={styles.copy}>
               At Nier Transportation, we&#39;re more than a car service;
               we&#39;re your trusted partner in luxury transportation. With a
               passion for providing the best car service in Phoenix and
@@ -24,6 +28,50 @@ const AboutUsSection = () => {
               service near you while ensuring your comfort, safety, and peace of
               mind.
             </p>
+            <div className={styles.iconTextContainer}>
+              <div className={styles.itextLeft}>
+                <Driver width={50} height={50} />
+              </div>
+              <div className={styles.itexRight}>
+                <strong className={styles.strong}>
+                  Professional Chauffeurs
+                </strong>
+                <p className={styles.copy}>
+                  Our experienced and courteous chauffeurs are the epitome of
+                  professionalism, providing you with a seamless journey from
+                  start to finish.
+                </p>
+              </div>
+            </div>
+            <div className={styles.iconTextContainer}>
+              <div className={styles.itextLeft}>
+                <Options width={50} height={50} />
+              </div>
+              <div className={styles.itexRight}>
+                <strong className={styles.strong}>Diverse Services</strong>
+                <p className={styles.copy}>
+                  Whether you require airport transfers, city tours, or
+                  executive transportation, we have tailored services to suit
+                  every need.
+                </p>
+              </div>
+            </div>
+            <div className={styles.iconTextContainer}>
+              <div className={styles.itextLeft}>
+                <Diamond width={50} height={50} />
+              </div>
+              <div className={styles.itexRight}>
+                <strong className={styles.strong}>Timeless Elegance</strong>
+                <p className={styles.copy}>
+                  Our fleet of meticulously maintained black cars exudes class
+                  and style, ensuring that you arrive at your destination not
+                  just comfortably, but with an air of distinction.
+                </p>
+              </div>
+            </div>
+            <div className={styles.btnContainer}>
+              <Button href='/' text='More about us' btnType='primary' />
+            </div>
           </div>
         </div>
       </section>
