4860966906f12c38d6f7e55f3e29c7d445e799cc
Chris Ware <c.p.ware@hotmail.com>
2023-09-11 23:38:12 -0700

    ContactFormEmail component

---
+++ email/contact-form-email.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type ContactFormEmailProps = {
+  message: string;
+};
+
+export default function ContactFormEmail({ message }: ContactFormEmailProps) {
+  return (
+    <div>
+      <h1>Contact Form Email</h1>
+    </div>
+  );
+}
