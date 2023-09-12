8b4e0c7adc08ffaf87319450009245f515107fe9
Chris Ware <c.p.ware@hotmail.com>
2023-09-12 09:46:59 -0700

    new data method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,31 +7,25 @@ import BookingProcess from "./components/BookingProcess/BookingProcess";
 import ContactSection from "./components/ContactSection/ContactSection";
 import TestimonialSection from "./components/TestimonialSection/TestimonialSection";
 import FaqSection from "./components/Faqs/FaqSection";
-// import fs from "fs";
-// import path from "path";
-// import matter from "gray-matter";
-// import BlogSection from "./components/BlogSection/BlogSection";
+import BlogSection from "./components/BlogSection/BlogSection";
 import FinalCTA from "./components/FinalCTA/FinalCTA";
 
 export default function Home() {
-  // const blogDir = "blogs";
-  // const files = fs.readdirSync(path.join(blogDir));
-  // const blogs = files.map((filename) => {
-  //   try {
-  //     const fileContent = fs.readFileSync(
-  //       path.join(blogDir, filename),
-  //       "utf-8"
-  //     );
-  //     const { data: frontMatter } = matter(fileContent);
-  //     return {
-  //       meta: frontMatter,
-  //       slug: filename.replace(".mdx", ""),
-  //     };
-  //   } catch (error) {
-  //     console.error(`Error reading or parsing file: ${filename}`, error);
-  //     return null; // or handle the error in an appropriate way
-  //   }
-  // });
+  const fs = require("fs");
+  const path = require("path");
+  const matter = require("gray-matter");
+
+  const files = fs.readdirSync(path.join("blogs"));
+
+  const blogs = files.map((filename: string) => {
+    const fileContent = fs.readFileSync(path.join("blogs", filename), "utf-8");
+
+    const { data: frontMatter } = matter(fileContent);
+    return {
+      meta: frontMatter,
+      slug: filename.replace(".mdx", ""),
+    };
+  });
 
   return (
     <main>
@@ -43,7 +37,7 @@ export default function Home() {
       <BookingProcess />
       <TestimonialSection />
       <FaqSection />
-      {/* <BlogSection blogData={blogs} /> */}
+      <BlogSection blogData={blogs} />
       <ContactSection />
       <FinalCTA />
     </main>
