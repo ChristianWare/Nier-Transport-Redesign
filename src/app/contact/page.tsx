d95f7cb86e3e1be1801c2d0f6086cfcb09058a4d
Chris Ware <c.p.ware@hotmail.com>
2023-09-13 12:32:08 -0700

    Added Map in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,6 +5,8 @@ import Contact2 from "../../../public/images/contact2.png";
 import FinalCTA from "../components/FinalCTA/FinalCTA";
 import ContactSection from "../components/ContactSection/ContactSection";
 import BlogSection from "../components/BlogSection/BlogSection";
+import FaqSection from "../components/Faqs/FaqSection";
+import styles from "./Contactpage.module.css";
 
 const ContactPage = () => {
   const fs = require("fs");
@@ -32,6 +34,20 @@ const ContactPage = () => {
       />
 
       <ContactSection />
+      <LayoutWrapper>
+        <ContentPadding>
+          <iframe
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.248717288147!2d-111.86333289999999!3d33.572891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872b7553fa7aaaab%3A0x55c463417f2cb0a2!2s10105%20E%20V%C3%ADa%20Linda%20Suite%20A-%20105%2C%20Scottsdale%2C%20AZ%2085258!5e0!3m2!1sen!2sus!4v1694633298828!5m2!1sen!2sus'
+            width='100%'
+            height='450'
+            allowFullScreen={true}
+            loading='lazy'
+            referrerPolicy='no-referrer-when-downgrade'
+            className={styles.test}
+          ></iframe>
+        </ContentPadding>
+      </LayoutWrapper>
+      <FaqSection />
       <BlogSection blogData={blogs} />
       <FinalCTA />
     </main>
