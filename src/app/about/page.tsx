edaad936d9346866cb9cceed0885d2526a94b031
Chris Ware <c.p.ware@hotmail.com>
2023-09-12 21:37:54 -0700

    Added Fleet to about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,30 +12,31 @@ import ContactSection from "../components/ContactSection/ContactSection";
 import FinalCTA from "../components/FinalCTA/FinalCTA";
 import TestimonialSection from "../components/TestimonialSection/TestimonialSection";
 import Commitment from "../components/Commitment/Commitment";
+import FleetSection from "../components/FleetSection/FleetSection";
 
 const AboutPage = () => {
-    const fs = require("fs");
-    const path = require("path");
-    const matter = require("gray-matter");
+  const fs = require("fs");
+  const path = require("path");
+  const matter = require("gray-matter");
 
-    // Determine the correct path to the 'blogs' directory
-    const blogsDirectory = path.join(process.cwd(), "blogs");
+  // Determine the correct path to the 'blogs' directory
+  const blogsDirectory = path.join(process.cwd(), "blogs");
 
-    // Use readdirSync to list files in the 'blogs' directory
-    const files = fs.readdirSync(blogsDirectory);
+  // Use readdirSync to list files in the 'blogs' directory
+  const files = fs.readdirSync(blogsDirectory);
 
-    const blogs = files.map((filename: any) => {
-      const fileContent = fs.readFileSync(
-        path.join(blogsDirectory, filename),
-        "utf-8"
-      );
+  const blogs = files.map((filename: any) => {
+    const fileContent = fs.readFileSync(
+      path.join(blogsDirectory, filename),
+      "utf-8"
+    );
 
-      const { data: frontMatter } = matter(fileContent);
-      return {
-        meta: frontMatter,
-        slug: filename.replace(".mdx", ""),
-      };
-    });
+    const { data: frontMatter } = matter(fileContent);
+    return {
+      meta: frontMatter,
+      slug: filename.replace(".mdx", ""),
+    };
+  });
   return (
     <main>
       <PageIntro
@@ -47,6 +48,7 @@ const AboutPage = () => {
       <Commitment />
       <Mission />
       <ServicesSection />
+      <FleetSection />
       <TestimonialSection />
       <FaqSection />
       <BlogSection blogData={blogs} />
