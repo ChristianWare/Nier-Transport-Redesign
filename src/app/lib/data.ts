3025a225f93e72c80e470e1d178eafb2d0b6f1cf
Chris Ware <c.p.ware@hotmail.com>
2023-09-14 14:01:43 -0700

    Added testimonials in datafile

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -128,65 +128,65 @@ export const reviews = [
   {
     id: 7,
     review:
-      "I rely on Nier Transportation for all my business trips. Their professionalism, punctuality, and luxurious vehicles make every journey a breeze. Highly recommended!",
-    reviewer: "John S.",
-    company: "Business Traveler",
+      "Nier Transportation provided the best, outstanding quality of service for at least 20-25 family members for memorial services for my dear father. We required multiple stops, and wait times, and they were first of all affordable, on time, professional, and extremely kind. I would 1000% recommend using them for any transportation needs for personal and business purposes.",
+    reviewer: "Linda R.",
+    company: "Gilbert, AZ",
     person: Person,
   },
   {
     id: 8,
     review:
-      "Our wedding day was made even more special with Nier Transportation. The elegant black car service added a touch of sophistication to our celebration. Thank you for making it unforgettable!",
-    reviewer: "Sara M.",
-    company: "Wedding Party",
+      "Barry provides great reliable service in comfortable luxury. I travel out 1-2 times/month and I can schedule ahead. Barry will always verify the day before in the event anything changes (which it can with my job). I feel safer having someone I know pick me up sometimes at 4a. Thank you Barry!",
+    reviewer: "Sheryl G.",
+    company: "Scottsdale, AZy",
     person: Person,
   },
   {
     id: 9,
     review:
-      "As a frequent traveler, I've tried many car services, but Nier Transportation stands out. Their consistency and top-notch service keep me coming back every time.",
-    reviewer: "David R.",
-    company: "Frequent Traveler",
-    person: Person,
-  },
-  {
-    id: 10,
-    review:
-      "I always choose Nier Transportation for airport transfers. They've never let me down, and the peace of mind knowing they'll be waiting, even if my flight is delayed, is priceless.",
-    reviewer: "Emily R.",
-    company: "Airport Transfer",
+      "I use Nier Transportation weekly for business travel and occasionally for personal trips.  They are always timely, super friendly, and helpful, all at reasonable rates.  I highly recommend them!",
+    reviewer: "Jeff G.",
+    company: "Sausalito, CA",
     person: Person,
   },
   {
     id: 11,
     review:
-      "We celebrated our anniversary with Nier Transportation, and it was a romantic and unforgettable experience. The luxurious ride and attentive driver made it truly special.",
-    reviewer: "Mark and Lisa H.",
-    company: "Anniversary Celebration",
+      "We have used this car service 4 times in the last few months. Booking a pick-up/drop-off time is very easy and seamless. Barry, the owner is very professional, friendly, and prompt as was one of his other drivers. We have flown in and out of Phoenix all 4 times with our dog and his kennel and Barry was very attentive to ensuring our dog (while in his kennel) was comfortable and safe before we left.Pricing is competitive but the service is better and good value for what you get. Cars are very roomy and spotless. Highly recommend this car service.",
+    reviewer: "Cheryl C.",
+    company: "Northwest Calgary, Canada",
     person: Person,
   },
   {
     id: 12,
     review:
-      "I regularly book Nier Transportation for our corporate events. Their professionalism, reliability, and attention to detail have been instrumental in our event's success.",
-    reviewer: "Jessica M.",
-    company: " Event Coordinator,",
+      "We first rode with Barry over one year ago.We were so impressed with his level of pride, professional bearing, situational awareness, and meticulous attention to detail in all mannersrelating to our safety and satisfaction that we made the decision to use him exclusively for all our transportation needs.Nier Transportation has afforded us the luxury of worry-free rides with absolute dependability. They monitor our arrival and departure schedules in real time and adjust accordingly, with no excuses, cancellations, or surprises.Their level of service and the peace of mind it gives is priceless.",
+    reviewer: "Jim & Connie A.",
+    company: "Phoenix, AZ,",
     person: Person,
   },
   {
     id: 13,
     review:
-      "I recently used Nier Transportation for a long-distance trip, and it was a comfortable and stress-free journey. Their spacious vehicles and courteous drivers made the miles fly by.",
-    reviewer: "Michael B. ",
-    company: "Long-Distance Travel",
+      "We used Nier Transportation to start and end our European vacation. It was the BEST decision! Our driver was professional, friendly, and timely. We also felt as though we were riding in luxury, the vehicle was new, fresh, and comfortable! An extra thanks for being at the airport waiting for us when we arrived back home at 1:30 a.m.!",
+    reviewer: "Lynn B",
+    company: "Tempe, AZ",
     person: Person,
   },
   {
     id: 145,
     review:
-      "Our family vacation was made easier with Nier Transportation. Their friendly and accommodating service ensured our family's comfort throughout our trip. We'll definitely book again!",
-    reviewer: "Linda T. ",
-    company: "Family Vacation",
+      "Have used this service multiple times. The drivers are great. Always very professional and prompt. You can tell they care about safety and a great customer experience, would definitely recommend it.",
+    reviewer: "Illeana L.",
+    company: "Mesa, AZ",
+    person: Person,
+  },
+  {
+    id: 146,
+    review:
+      "I just started utilizing Nier Transportation a couple of months ago and have never used a driving service before. I was surprised to see how easy it was to schedule and use as I wasn't sure how it would work in relation to price timeliness, and reliability. I travel almost weekly and Barry, the owner made it very simple by asking for my flight itinerary, and pick-up times. I have used his service a few times so far and have had numerous changes to my plans and he has come through every time. He even made a special trip to pick up a backpack for my daughter when I was in Hawaii so when we returned, we wouldn't have to make a special trip. Vehicles are nice and clean, and Barry is a good man. His pricing is competitive and you get great value with all the things I stated above. Highly Recommended",
+    reviewer: "Adam B.",
+    company: "Phoenix, AZ",
     person: Person,
   },
 ] as const;
