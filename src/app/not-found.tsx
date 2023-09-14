f786d5bd6d146c1b82ec29f94b549920c7b1f2bc
Chris Ware <c.p.ware@hotmail.com>
2023-09-14 15:52:43 -0700

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,10 @@
 import PageIntro from "./components/PageIntro/PageIntro";
 import Img from "../../public/images/barry.jpg";
 import Link from "next/link";
+import FaqSection from "./components/Faqs/FaqSection";
+import FinalCTA from "./components/FinalCTA/FinalCTA";
+import ContactSection from "./components/ContactSection/ContactSection";
+import AboutUsSection from "./components/AboutUsSection/AboutUsSection";
 
 export default function NotFound() {
   return (
