ffedf43034a7c702c8ae9a79ad758bd702583980
Chris Ware <c.p.ware@hotmail.com>
2024-06-19 20:08:10 -0700

    route fix

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-// const SEND_EMAIL = "chris.ware.dev@gmail.com";
-const SEND_EMAIL = "reservations@niertransportation.com";
+const SEND_EMAIL = "chris.ware.dev@gmail.com";
+// const SEND_EMAIL = "reservations@niertransportation.com";
 const PASSWORD = "eayw aboq lrso ugqx";
-const ADDITIONAL_EMAIL = "chris.ware.dev@gmail.com"; // Add the second email address here
+const ADDITIONAL_EMAIL = "reservations@niertransportation.com"; // Add the second email address here
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -18,10 +18,10 @@ const transporter = nodemailer.createTransport({
 export async function POST(request: Request) {
   const data = await request.json();
   const info = await transporter.sendMail({
-    from: `"Nier Transportation Contact Form Submission" <${SEND_EMAIL}>`, // sender name and address
-    to: `${SEND_EMAIL}, ${ADDITIONAL_EMAIL}`, // list of receivers
+    from: `"Nier Transportation Contact Form Submission" <${ADDITIONAL_EMAIL}>`, // sender name and address
+    to: `${ADDITIONAL_EMAIL}`, // list of receivers
     replyTo: data.senderEmail, // reply-to address
-    subject: "Portfolio Contact Form Submission", // Subject line
+    subject: "You have a new message!", // Subject line
     text: "Hello world?",
     html: `<b>First Name:</b> <br/> 
             ${data?.firstName} <br/> <br/>
